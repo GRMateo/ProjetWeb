2a6987f95b0631e59f57b7f26fc8333d5df11023
Norgh <pierre.sen@hotmail.fr>
2021-03-31 12:04:22 +0200

    Almost over

--- Javascript/creationPlateau.js
+++ Javascript/creationPlateau.js
@@ -7,6 +7,69 @@ class GameJoueur {
         this.click2 = [];
         this.tab_size = [1,1,1,2,3,4,4,4,5,8,1,6];
         this.listeneur(); // We call the listeners
+        this.generationAleatoire();
+    }
+
+    getRandomInt(min,max){
+        return Math.trunc(Math.random() * (max - min) + min);
+    }
+
+    generationAleatoire(){
+        let div= document.getElementById("boutonAleat");
+        let submit = document.createElement("button");
+        submit.id = "boutonAleat";
+        submit.value="Aléatoire";
+        div.appendChild(submit);
+        let sub = document.getElementById("boutonAleat");
+        sub.addEventListener('click', () => { 
+            console.log("Génération aléatoire");
+            this.pionsRandoms();
+            //socket.emit();
+        });
+    }
+
+    pionsRandoms(){
+        let tab_size = [1,1,1,2,3,4,4,4,5,8,1,6];
+
+        for(let x = 0; x < 4; x++)
+        {
+            for(let y = 0; y < 10; y++)
+            {
+                let i;
+                do{
+                    i = this.getRandomInt(1,13);
+                    console.log("i: ", i);
+                    if(i==1) this.game.array1[x][y] = new Pions(1, 1, "drapeau", 0, 1, 0, 1, 1, 1);
+                    else if(i==2) this.game.array1[x][y] = new Pions(2, 1, "maréchal", 10, 1, 0, 1, 1, 0);
+                    else if(i==3) this.game.array1[x][y] = new Pions(3, 1, "général", 9, 1,0, 1, 1, 0);
+                    else if(i==4) this.game.array1[x][y] = new Pions(4, 2, "colonel", 8, 1, 0, 1, 1, 0);
+                    else if(i==5) this.game.array1[x][y] = new Pions(5, 3, "commandant", 7, 1, 0, 1, 1, 0);
+                    else if(i==6) this.game.array1[x][y] = new Pions(6, 4, "capitaine", 6, 1, 0, 1, 1, 0);
+                    else if(i==7) this.game.array1[x][y] = new Pions(7, 4, "lieutenant", 5, 1, 0, 1, 1, 0);
+                    else if(i==8) this.game.array1[x][y] = new Pions(8, 4, "sergent", 4, 1, 0, 1, 1, 0);
+                    else if(i==9) this.game.array1[x][y] = new Pions(9, 5, "démineur", 3, 1, 0, 1, 1, 0);
+                    else if(i==10) this.game.array1[x][y] = new Pions(10, 8, "éclaireur", 2, 1, 0, 1, 1, 0);
+                    else if(i==11) this.game.array1[x][y] = new Pions(11, 1, "espion", 1, 1, 0, 1, 1, 0);
+                    else if(i==12) this.game.array1[x][y] = new Pions(12, 6, "bombe", 11, 1, 0, 1, 1, 1);
+                    tab_size[i-1]-=1;
+                }
+                while(tab_size[i-1] < 0)
+            }
+        }
+        console.log(this.game.array1);
+        let tab = document.getElementById("plateau");
+        for(let x = 0; x < 4; x++)
+        {
+            for(let y = 0; y < 10; y++)
+            {
+                tab.rows[x].cells[y].className = "terre"+this.game.array1[x][y].Id;
+            }
+        }
+        let tab1 = document.getElementById("plateau1");
+        for (let j = 0; j < 12; j++) 
+        {
+            tab1.rows[0].cells[j].className="terre";
+        }
     }
 
     listeneur(){
@@ -99,13 +162,12 @@ class GameJoueur {
     }
 
     buttonSendGrid(){
-        //<input type="submit" value="Envoyer le formulaire">
         let div= document.getElementById("boutonEnvoi");
         let submit = document.createElement("button");
-        submit.id = "bouton";
+        submit.id = "boutonEnvoi";
         submit.value="Envoyer";
         div.appendChild(submit);
-        let sub = document.getElementById("bouton");
+        let sub = document.getElementById("boutonEnvoi");
         let appuiBouton=0;
         sub.addEventListener('click', () => { 
             if(appuiBouton ==0) 
--- Javascript/plateau.js
+++ Javascript/plateau.js
@@ -19,34 +19,26 @@ class plateau{
     }
 
     placePions(){
-
-        let tab_size = [1,1,1,2,3,4,4,4,5,8,1,6];
-
+        let tabJ1=[[],[],[],[],[],[],[],[],[],[]]; // Tableau joueur 1 (on attend le socket du serveur hein)
+        let tabJ2=[[],[],[],[],[],[],[],[],[],[]]; // Tableau joueur 2 (on attend le socket du serveur hein)
         for(let x = 0; x < 4; x++)
         {
             for(let y = 0; y < 10; y++)
             {
-                let i;
-                do{
-                    i = this.getRandomInt(1,13);
-                    if(i==1) this.array1[x][y] = new Pions(1, 1, "drapeau", 0, 1, 0, 1, 1, 1);
-                    else if(i==2) this.array1[x][y] = new Pions(2, 1, "maréchal", 10, 1, 0, 1, 1, 0);
-                    else if(i==3) this.array1[x][y] = new Pions(3, 1, "général", 9, 1,0, 1, 1, 0);
-                    else if(i==4) this.array1[x][y] = new Pions(4, 2, "colonel", 8, 1, 0, 1, 1, 0);
-                    else if(i==5) this.array1[x][y] = new Pions(5, 3, "commandant", 7, 1, 0, 1, 1, 0);
-                    else if(i==6) this.array1[x][y] = new Pions(6, 4, "capitaine", 6, 1, 0, 1, 1, 0);
-                    else if(i==7) this.array1[x][y] = new Pions(7, 4, "lieutenant", 5, 1, 0, 1, 1, 0);
-                    else if(i==8) this.array1[x][y] = new Pions(8, 4, "sergent", 4, 1, 0, 1, 1, 0);
-                    else if(i==9) this.array1[x][y] = new Pions(9, 5, "démineur", 3, 1, 0, 1, 1, 0);
-                    else if(i==10) this.array1[x][y] = new Pions(10, 8, "éclaireur", 2, 1, 0, 1, 1, 0);
-                    else if(i==11) this.array1[x][y] = new Pions(11, 1, "espion", 1, 1, 0, 1, 1, 0);
-                    else if(i==12) this.array1[x][y] = new Pions(12, 6, "bombe", 11, 1, 0, 1, 1, 1);
-                    tab_size[i-1]-=1;
-                }
-                while(tab_size[i-1] < 0)
+                tabJ1[x][y] = new Pions(0, -1, "Satan", -1, -1, -1, -1, -1, -1);
+                tabJ2[x][y] = new Pions(0, -1, "Satan", -1, -1, -1, -1, -1, -1);
             }
         }
-
+        console.log(tabJ1);
+        console.log(tabJ2);
+        // First Player's pawns
+        for(let x = 0; x < 4; x++)
+        {
+            for(let y = 0; y < 10; y++)
+            {
+                this.array1[x][y] = tabJ1[x][y];
+            }
+        }
         // Middle
         for(let x = 4; x < 6; x++)
         {
@@ -56,29 +48,11 @@ class plateau{
             }
         }
         // Second Player's pawns
-        tab_size = [1,1,1,2,3,4,4,4,5,8,1,6];
         for(let x = 6; x < 10; x++)
         {
             for(let y = 0; y < 10; y++)
             {
-                let i;
-                do{
-                    i = this.getRandomInt(1,13);
-                    if(i==1) this.array1[x][y] = new Pions(1, 1, "drapeau", 0, 0, 1, 1, 1, 1);
-                    else if(i==2) this.array1[x][y] = new Pions(2, 1, "maréchal", 10, 0, 1, 1, 1, 0);
-                    else if(i==3) this.array1[x][y] = new Pions(3, 1, "général", 9, 0, 1, 1, 1, 0);
-                    else if(i==4) this.array1[x][y] = new Pions(4, 2, "colonel", 8, 0, 1, 1, 1, 0);
-                    else if(i==5) this.array1[x][y] = new Pions(5, 3, "commandant", 7, 0, 1, 1, 1, 0);
-                    else if(i==6) this.array1[x][y] = new Pions(6, 4, "capitaine", 6, 0, 1, 1, 1, 0);
-                    else if(i==7) this.array1[x][y] = new Pions(7, 4, "lieutenant", 5, 0, 1, 1, 1, 0);
-                    else if(i==8) this.array1[x][y] = new Pions(8, 4, "sergent", 4, 0, 1, 1, 1, 0);
-                    else if(i==9) this.array1[x][y] = new Pions(9, 5, "démineur", 3, 0, 1, 1, 1, 0);
-                    else if(i==10) this.array1[x][y] = new Pions(10, 8, "éclaireur", 2, 0, 1, 1, 1, 0);
-                    else if(i==11) this.array1[x][y] = new Pions(11, 1, "espion", 1, 0, 1, 1, 1, 0);
-                    else if(i==12) this.array1[x][y] = new Pions(12, 6, "bombe", 11, 0, 1, 1, 1, 1);
-                    tab_size[i-1]-=1;
-                }
-                while(tab_size[i-1] < 0)
+                this.array1[x][y] = tabJ2[x-6][y];
             }
         }
         let plateau=document.getElementById("plateau");
